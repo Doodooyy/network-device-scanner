9ec7a24edcde055090434e647e7711d0435fc79d
Doodooyy <aryamanaggr@gmail.com>
2026-02-02 20:21:09 +0530

    Added option to choose interface, no need to provide subnet or manually put in the mask

--- scanner.py
+++ scanner.py
@@ -1,19 +1,45 @@
 from scapy.all import Ether, ARP, srp
+from datetime import datetime, timezone
+import netifaces
 import sys
-def ARP_scan(subnet):
+import ipaddress
+def ARP_scan(subnet,iface):
     packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=subnet)
-    answered = srp(packet,timeout=2)[0]
+    answered = srp(packet,iface=iface,timeout=2)[0]
     devices = []
     for sent,received in answered:
         devices.append({"ip":received.psrc,"mac":received.hwsrc})
     return devices
 
+def get_interface_info(iface):
+    addresses = netifaces.ifaddresses(iface)
+
+    if netifaces.AF_INET not in addresses:
+        return None  
+
+    return addresses[netifaces.AF_INET][0]["addr"],addresses[netifaces.AF_INET][0]["netmask"]
+
 if __name__ == "__main__":
     if(len(sys.argv)!=2):
         sys.exit(1)
-    subnet = sys.argv[1]
-    devices = ARP_scan(subnet)
+    iface = sys.argv[1]
+
+    info = get_interface_info(iface)
+    if info is None:
+        sys.exit(1)
+    interface_ip, netmask = info
+    subnet = str(ipaddress.IPv4Network(f"{interface_ip}/{netmask}", strict=False) )
+    devices = ARP_scan(subnet,iface)
     print("Found",len(devices),"Devices")
     for device in devices:
         print("IP: ",device["ip"])
         print("MAC: ",device["mac"])
+    timestamp = datetime.now(timezone.utc).isoformat()
+    network_snapshot = {"meta":{
+        "interface":iface,
+        "interface_ip":interface_ip,
+        "subnet":subnet,
+        "timestamp":timestamp
+        },
+        "devices":devices
+    }
